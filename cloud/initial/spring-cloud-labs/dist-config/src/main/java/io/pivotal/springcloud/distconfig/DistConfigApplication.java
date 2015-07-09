b81c480867aef825e2a305780581a08eb1a80702
rkrishna <rkrishna@192.168.100.70>
2015-07-09 19:58:49 -0400

    Config client check in

--- cloud/initial/spring-cloud-labs/dist-config/src/main/java/io/pivotal/springcloud/distconfig/DistConfigApplication.java
+++ cloud/initial/spring-cloud-labs/dist-config/src/main/java/io/pivotal/springcloud/distconfig/DistConfigApplication.java
@@ -1,12 +1,22 @@
 package io.pivotal.springcloud.distconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class DistConfigApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DistConfigApplication.class, args);
+    }
+    @Autowired
+    private Greeter greeter;
+    
+    @RequestMapping("/")
+    public String home() {
+        return String.format("%s World!", greeter.getGreeting());
     }
 }
