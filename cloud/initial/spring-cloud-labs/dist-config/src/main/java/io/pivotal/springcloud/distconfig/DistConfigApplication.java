3ee74876da8c17f87031fbc531841372836e9a43
rkrishna <rkrishna@192.168.100.70>
2015-07-09 20:16:53 -0400

    changes spring cloud client

--- cloud/initial/spring-cloud-labs/dist-config/src/main/java/io/pivotal/springcloud/distconfig/DistConfigApplication.java
+++ cloud/initial/spring-cloud-labs/dist-config/src/main/java/io/pivotal/springcloud/distconfig/DistConfigApplication.java
@@ -5,18 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
+
 @SpringBootApplication
+@RestController
 public class DistConfigApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DistConfigApplication.class, args);
-    }
     @Autowired
     private Greeter greeter;
-    
+
     @RequestMapping("/")
     public String home() {
         return String.format("%s World!", greeter.getGreeting());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistConfigApplication.class, args);
     }
 }
